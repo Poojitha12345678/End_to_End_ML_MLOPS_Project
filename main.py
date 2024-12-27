96c920fa182b4361e89b96c5538bb95adf0d8c05
Poojitha Pulipati <pulipatipoojitha@gmail.com>
2024-12-27 19:28:24 +0530

    Model Evaluation with ML flow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from MLFlow_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from MLFlow_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLFlow_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from MLFlow_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from MLFlow_Project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
@@ -40,6 +41,16 @@ STAGE_NAME = 'Model training Stage'
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = 'Model Evaluation Stage'
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelEvaluationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
--- src/MLFlow_Project/config/configuration.py
+++ src/MLFlow_Project/config/configuration.py
@@ -1,6 +1,6 @@
 from MLFlow_Project.constants import *
 from MLFlow_Project.utils.common import read_yaml , create_directories
-from MLFlow_Project.entity.config_entity import DataIngestionConfig ,DataValidationConfig ,DataTransformationConfig ,ModelTrainerConfig
+from MLFlow_Project.entity.config_entity import DataIngestionConfig ,DataValidationConfig ,DataTransformationConfig , ModelTrainerConfig , ModelEvaluationConfig
 
 
 class ConfigurationManager:
@@ -79,3 +79,23 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/Poojitha12345678/End_to_End_ML_MLOPS_Project.mlflow",
+           
+        )
+
+        return model_evaluation_config
