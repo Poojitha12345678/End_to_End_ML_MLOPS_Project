50d5b702f20bfc2f5400ec3c06d3edb0901709dd
Poojitha Pulipati <pulipatipoojitha@gmail.com>
2024-12-27 18:09:21 +0530

    Model Traning completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from MLFlow_Project import logger
 from MLFlow_Project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLFlow_Project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from MLFlow_Project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLFlow_Project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
 STAGE_NAME = 'Data Ingestion Stage'
 
 try:
@@ -28,6 +30,16 @@ STAGE_NAME = 'Data Transformation Stage'
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = 'Model training Stage'
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
